e4571d98ba233801f03e6529373a6de73cfd6021
Brett R. Gebbie <brg078@gmail.com>
2022-10-25 14:17:47 -0500

    whoops one mega commit

--- client.js
+++ client.js
@@ -48,10 +48,76 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
+//function to determine bonus for given employee array
+function processBonuses(staffArray) {
+  let outputArray = [];
+  for (let worker of staffArray) {
+    //console.log('Processing bonus for: ', worker.name);
+    console.log(bonusCalculation(worker));
+    outputArray.push(bonusCalculation(worker));
+  }
+  document.getElementById("results").textContent=outputArray;
+  return outputArray;
+}
+
+window.onload = processBonuses(employees);
+
+function bonusCalculation(employee) {
+  let bonusCalc = {name: employee.name};
+  //console.log('Bonus Calc for:', employee.name);
+  if(employee.reviewRating <= 2){
+    //console.log(employee.name, 'does not get a bonus.');
+    bonusCalc.bonusPercentage = 0;
+    bonusCalc.totalBonus = 0;
+    bonusCalc.totalCompensation = parseInt(employee.annualSalary);
+    //console.log(employee.name + `'s annual salary is:`, bonusCalc.totalCompensation);
+    //console.log(bonusCalc);
+    return bonusCalc;
+  }
+  else if (employee.annualSalary > 65000){
+    //console.log(employee.name, 'only one percent!?, even with this solid review');
+    bonusCalc.bonusPercentage = 1;
+    bonusCalc.totalBonus = Math.floor(employee.annualSalary * (bonusCalc.bonusPercentage / 100));
+    //console.log(bonusCalc.totalBonus, ' is my bonus, very weak I quit');
+    bonusCalc.totalCompensation = parseInt(employee.annualSalary) + bonusCalc.totalBonus;
+    //console.log(employee.annualSalary, 'this is what i was making');
+    //console.log(bonusCalc.totalCompensation, 'i guess this is how much you make now')
+    //console.log(bonusCalc);
+    return bonusCalc;
+  }
+  else {
+    let baseBonus = 4;
+    //console.log('higher performers');
+    if(employee.reviewRating === 4){
+      baseBonus += 2;
+      //console.log('My review score is 4');
+    } else if(employee.reviewRating === 5){
+      baseBonus += 6;
+      //console.log('My review score is 5');
+    }
+    if(employee.employeeNumber.length == 4){
+      //console.log('I have a 4 digit Employee ID');
+      baseBonus += 5;
+    }
+    if(baseBonus > 13){
+      baseBonus = 13;
+      //console.log('I am too good, reduce my bonus to 13%');
+    }
+    bonusCalc.bonusPercentage = baseBonus;
+    bonusCalc.totalBonus = Math.floor(employee.annualSalary * (bonusCalc.bonusPercentage / 100));
+    bonusCalc.totalCompensation = parseInt(employee.annualSalary) + bonusCalc.totalBonus;
+    //console.log(bonusCalc);
+    return bonusCalc;
+  }
+}
+
+//calling function
+processBonuses(employees);
+
+//function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
   
   
   // return new object with bonus results
 
-}
+//}  We made our own function name.
